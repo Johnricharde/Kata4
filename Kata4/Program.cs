fa3657da04c51250d98666ee15bf7b52abffae05
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2024-01-13 17:32:13 +0100

    Add project files.

---
+++ Kata4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System;
+
+namespace Kata4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public static string HighAndLow(string numbers)
+        {
+            int[] intArray = numbers.Split(" ").Select(int.Parse).ToArray();
+
+            int highest = Convert.ToInt32(intArray.Max());
+            int lowest = Convert.ToInt32(intArray.Min());
+
+            return String.Join(" ", highest, lowest);
+        }
+    }
+}
